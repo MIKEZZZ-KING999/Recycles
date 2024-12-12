29879fdd4fa1fd93cbdbe9286e2d58df37979f15
MIKEZZZ-KING999 <micigwubor@gmail.com>
2024-12-12 17:54:15 +0000

    Add files via upload

---
+++ recycle.py
@@ -0,0 +1,12 @@
+import pgzrun
+import random
+
+FONT_option = (255, 255, 255)
+WIDTH = 800
+HEIGHT = 600
+CENTRE_X = WIDTH / 2
+CENTRE_Y = HEIGHT / 2
+CENTRE = (CENTRE_X, CENTRE_Y)
+FINAL_LEVEL = 6
+START_SPEED = 10
+ITEMS = ["bag", "battery", "bottle", "chips"]
